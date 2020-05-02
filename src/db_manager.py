5c7d0e06e9d6ca5a88acce0569fa6de0a5a1d847
Dimitar Terzov <dimiterat@gmail.com>
2020-05-02 14:04:03 +0300

    Create database manager.

---
+++ src/db_manager.py
@@ -0,0 +1,27 @@
+from db_client import db_client as client
+from tables_magaer import TablesManager
+from db_tables import *
+
+
+app = App()
+sandbox = Sandbox()
+app_ver = AppVer()
+analysis = Analysis()
+analysis_unint = AnalysisUnit()
+engine_job = EngineJob()
+analysis_unit_dyn_op = AnalysisUnitDynOp()
+analysis_unit_dyn_params = AnalysisUnitDynParams()
+scan_encrypt = ScanEncrypt()
+analysis_unit_scan_window = AnalysisUnitScanWindow()
+
+manager = TablesManager(client, app, sandbox, app_ver, analysis,
+                analysis_unint, engine_job, analysis_unit_dyn_op,
+                analysis_unit_dyn_params, scan_encrypt, analysis_unit_scan_window)
+
+def main(manager):
+    manager.set_new_ids()
+    manager.insert_into_tables()
+
+
+if __name__ == '__main__':
+    main(manager)
