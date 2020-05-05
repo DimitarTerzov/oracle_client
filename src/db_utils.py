3362aeef11f19cac981ff90312d9fd9a363fe52d
Dimitar Terzov <dimiterat@gmail.com>
2020-05-05 16:46:11 +0300

    Fix helper method.

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,9 +1,11 @@
 def params_as_list(params_count):
     """
-    _params_as_list(params_count=integer) -> string like "(?, ?, ...)"
+    params_as_list(params_count=integer) -> string like "(:1, :2, ...)"
 
-    Returns a string with as many ? as `params_count`.
+    Returns a string with as many place holders (:1) as `params_count`.
     """
-    placeholder_list = ["?"] * params_count
+    placeholder_list = []
+    for i in range(1, params_count+1):
+        placeholder_list.append(':{}'.format(i))
     return "({})".format(", ".join(placeholder_list))
 
